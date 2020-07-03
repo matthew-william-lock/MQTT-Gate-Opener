5b824c59ab8e5ceaf6e66d56d2063ce8f921ec26
Matthew Lock <matthewwilliamlock@gmail.com>
2020-07-03 22:58:38 +0200

    Added rounded button, loading spinner. Now acquires persisting message from mqtt broker for gate status

--- android_application/app/src/main/java/com/example/gateopener/MainActivity.java
+++ android_application/app/src/main/java/com/example/gateopener/MainActivity.java
@@ -2,39 +2,53 @@ package com.example.gateopener;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 
-import android.os.SystemClock;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Topics
+    public static final String GATE_COMMAND_TOPIC = "Hexdro/Gate/Command";
+    public static final String GATE_STATUS_TOPIC = "Hexdro/Gate/Status";
+    public static final String GATE_OPENING_STATUS = "opening";
+    public static final String GATE_CLOSING_STATUS = "closing";
+
+    // UI
+    Button gateButon;
+    private ProgressBar spinner;
 
     private Context mContext=MainActivity.this;
     private static final int REQUEST = 112;
@@ -46,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
     private MqttConnectOptions mqttConnectOptions;
 
     Boolean state;
+    Boolean firstStart=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,29 +71,19 @@ public class MainActivity extends AppCompatActivity {
 
         state=false;
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // UI
+        gateButon = findViewById(R.id.gate_btn);
+        gateButon.setVisibility(View.INVISIBLE);
+        spinner=(ProgressBar)findViewById(R.id.loading_spinner);
+        spinner.setVisibility(View.VISIBLE);
+
+//        FloatingActionButton fab = findViewById(R.id.fab);
+
+        gateButon.setEnabled(false);
+        gateButon.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                // Only perform action if init is successful
-                if (client!=null) {
-                    String topic = "Hexdro/LED";
-                    state=!state;
-                    String payload;
-                    if (state) payload="on";
-                    else payload = "off";
-                    byte[] encodedPayload = new byte[0];
-                    try {
-                        encodedPayload = payload.getBytes("UTF-8");
-                        MqttMessage message = new MqttMessage(encodedPayload);
-                        client.publish(topic, message);
-                        Toast.makeText(MainActivity.this,payload,Toast.LENGTH_SHORT).show();
-                    } catch (UnsupportedEncodingException | MqttException e) {
-                        Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
-                    }
-                }
-
+            public void onClick(View v) {
+                toggleGate();
             }
         });
 
@@ -107,6 +112,48 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Disconnect mqtt
+        try {
+            client.disconnect();
+        } catch (MqttException e) {
+            Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+
+        // Disable and hide button
+        gateButon.setEnabled(false);
+        gateButon.setVisibility(View.INVISIBLE);
+        firstStart=true;
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        spinner.setVisibility(View.VISIBLE);
+        try{
+            token = client.connect(mqttConnectOptions);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+//                    Toast.makeText(MainActivity.this,"Connected! "+client.isConnected(),Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Toast.makeText(MainActivity.this,"Failed to connect.",Toast.LENGTH_SHORT).show();
+                }
+
+            });
+        }
+        catch (MqttException e){
+            Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -164,15 +211,54 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void connectComplete(boolean reconnect, String serverURI) {
                 Toast.makeText(MainActivity.this,"Connected!",Toast.LENGTH_SHORT).show();
+                subscribe();
+
+                // Enable button
+                gateButon.setEnabled(true);
             }
 
             @Override
             public void connectionLost(Throwable cause) {
+                // Disable button
+                gateButon.setEnabled(false);
+
                 Toast.makeText(MainActivity.this,"Lost Connection...",Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void messageArrived(String topic, MqttMessage message) throws Exception {
+
+//                Toast.makeText(MainActivity.this, "Message on : "+topic,Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, message.toString(),Toast.LENGTH_SHORT).show();
+
+                if (topic.equals(GATE_COMMAND_TOPIC) ){
+//                    JSONObject reader = new JSONObject(message.toString());
+//                    Toast.makeText(MainActivity.this, message.toString(),Toast.LENGTH_SHORT).show();
+                }
+
+                else if (topic.equals(GATE_STATUS_TOPIC)) {
+                    JSONObject reader = new JSONObject(message.toString());
+                    JSONObject gate  = reader.getJSONObject("gate");
+
+                    String status = gate.getString("status");
+                    Toast.makeText(MainActivity.this, status,Toast.LENGTH_SHORT).show();
+
+                    if (status.equals(GATE_OPENING_STATUS)){
+                        Drawable background = getResources().getDrawable(R.drawable.roundedbuttongreen);
+                        gateButon.setBackground(background);
+                        gateButon.setEnabled(true);
+                        showGateButton();
+                    } else if(status.equals(GATE_CLOSING_STATUS)){
+                        Drawable background = getResources().getDrawable(R.drawable.roundedbuttonred);
+                        gateButon.setBackground(background);
+                        gateButon.setEnabled(true);
+                        showGateButton();
+                    }
+
+
+
+                }
+
 
             }
 
@@ -182,9 +268,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /* =============================================================================================================================================================
+        * Connect to Client
+        * ============================================================================================================================================================= */
 
-
-        // Connect
         try{
             token = client.connect(mqttConnectOptions);
             token.setActionCallback(new IMqttActionListener() {
@@ -204,6 +291,60 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    /* =============================================================================================================================================================
+     * Subscribe to status
+     * ============================================================================================================================================================= */
+    private void subscribe(){
+        int qos = 1;
+
+        try {
+            IMqttToken subToken = client.subscribe(GATE_STATUS_TOPIC, qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,Throwable exception) {
+                    Toast.makeText(MainActivity.this,"Could not connect to "+GATE_STATUS_TOPIC,Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (MqttException e) {
+            Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void toggleGate(){
+        gateButon.setEnabled(false);
+        if (client!=null) {
+            String topic = "Hexdro/Gate/Command";
+            String payload;
+//                    if (state) payload="on";
+//                    else payload = "off";
+            payload="toggle";
+            byte[] encodedPayload = new byte[0];
+            try {
+                encodedPayload = payload.getBytes("UTF-8");
+                MqttMessage message = new MqttMessage(encodedPayload);
+                client.publish(topic, message);
+                Toast.makeText(MainActivity.this,payload,Toast.LENGTH_SHORT).show();
+            } catch (UnsupportedEncodingException | MqttException e) {
+                Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void showGateButton(){
+        if (firstStart){
+            gateButon.setVisibility(View.VISIBLE);
+            Animation expandIn = AnimationUtils.loadAnimation(MainActivity.this,R.anim.fragment_open_enter);
+            gateButon.startAnimation(expandIn);
+            spinner.setVisibility(View.GONE);
+            firstStart=!firstStart;
+        }
     }
 
 }
